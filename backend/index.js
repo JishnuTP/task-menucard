93224de0fae9441feb2bf257a87e6fc3de8d6296
JishnuTP <tpjishnu5@gmail.com>
2024-07-26 23:50:36 +0530

    fghj

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,23 @@ connectDB();
 
 // Middlew
 // Middleware
-app.use(cors({
-    origin: "https://task-menucard-frontend.vercel.app", // Allow only this origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
-    credentials: true // Allow credentials if needed
-  }));
+// app.use(cors({
+//     origin: "https://task-menucard-frontend.vercel.app", // Allow only this origin
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
+//     credentials: true // Allow credentials if needed
+//   }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://task-menucard-frontend.vercel.app'); // Allow only this origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow these HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end(); // Respond to preflight requests
+    }
+    next();
+  });
+  
 app.use(express.json());
 
 // Test route
